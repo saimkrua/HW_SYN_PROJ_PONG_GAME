6e7d51eb5b41a209bfadbbbc02a3891b55158b2e
saimkrua <k.satapornimp@gmail.com>
2023-12-10 16:07:50 +0700

    [EDIT] clean main

--- Pong.srcs/sources_1/new/main.v
+++ Pong.srcs/sources_1/new/main.v
@@ -18,33 +18,46 @@ module main(
     wire [9:0] x,y;
     wire [3:0] movement;
     wire [11:0] rgb_out;
-    wire [19:0] clk_div;
     wire [8:0] scorePlayer1;
     wire [8:0] scorePlayer2;
+    wire [19:0] clk_div;
+
+    /*======================= IO =============================*/    
+    uart keyboardUART(clk, RsRx, RsTx, movement, throwBall); // keyboard input
+    /*================================================================*/
     
+    /*======================= GAMEINSTANCE =============================*/
+    gameLogic gameInstance(
+        clk, // in : clock
+        btnU,// in : reset
+        x,// in : position x from vga
+        y,// in : position y from vga
+        video_on, // in : show video from vga
+        movement, // in : from IO {player1Up, player1Down, player2Up, player2Down}
+        throwBall, // in : from IO
+        rgb_out, // out : for output 
+        scorePlayer1, // out : for 7 seg
+        scorePlayer2 // out : for 7 seg
+    );
+    /*================================================================*/
+    
+    
+    /*======================= VGA =============================*/
+    assign vgaBlue = rgb_out[3:0];
+    assign vgaGreen = rgb_out[7:4];
+    assign vgaRed = rgb_out[11:8];
+    
+    vga vga_render(.clk(clk), .reset(reset), .hsync(Hsync), .vsync(Vsync), .video_on(video_on), .p_tick(), .x(x), .y(y));
+    /*================================================================*/
+    
+    
+    /*======================= SEVEN SEGMENT =============================*/
     assign clk_div[0] = clk;
-    assign vgaBlue = {rgb_out[3],rgb_out[2],rgb_out[1],rgb_out[0]}; // assign rgb_out to vgaBlue, vgaGreen, vgaRed
-    assign vgaGreen = {rgb_out[7],rgb_out[6],rgb_out[5],rgb_out[4]};
-    assign vgaRed = {rgb_out[11],rgb_out[10],rgb_out[9],rgb_out[8]};
-    
-    generate for(genvar i = 0;i<19;i = i+1) begin // divine clock by 2^19 for 7 segment display
+    generate for(genvar i = 0;i<19;i = i+1) begin // divine clock by 2^19
         clock_divider div1(clk_div[i],clk_div[i+1]);
     end endgenerate
     
-    uart keyboardUART(clk, RsRx, RsTx, movement, throwBall); // keyboard input
-    // main game logic and animation logic
-    gameLogic gameInstance(
-        clk,
-        btnU,// reset
-        x,// position x
-        y,// position y
-        video_on, // videoOn show video where the value is 1
-        movement, // player1Up, player1Down, player2Up, player2Down
-        throwBall, // throwBall
-        rgb_out, // rgb output of the position x y
-        scorePlayer1,
-        scorePlayer2
-    );
-    vga vga_render(.clk(clk), .reset(reset), .hsync(Hsync), .vsync(Vsync), .video_on(video_on), .p_tick(), .x(x), .y(y)); // vga render
-    seven_segment_tdm segmentDisplay(clk_div[19],scorePlayer1[7:4],scorePlayer1[3:0],scorePlayer2[7:4],scorePlayer2[3:0],seg,an,1); // score 7 segment
+    seven_segment_tdm segmentDisplay(clk_div[19],scorePlayer1[7:4],scorePlayer1[3:0],scorePlayer2[7:4],scorePlayer2[3:0],seg,an,1);
+    /*================================================================*/
+    
 endmodule
